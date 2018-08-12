69286d791ebf564ba41413914d8094ab045f801c
Gavri Fernandez <gavri.fernandez@gmail.com>
2018-08-12 13:59:01 +0530

    Leap: Infix isDivisibleBy

--- leap/src/LeapYear.hs
+++ leap/src/LeapYear.hs
@@ -1,6 +1,6 @@
 module LeapYear (isLeapYear) where
 
-isDivisibleBy m n = mod m n == 0
+m `isDivisibleBy` n = mod m n == 0
 
 isLeapYear :: Integer -> Bool
-isLeapYear year = (isDivisibleBy year 4 && (not (isDivisibleBy year 100))) || isDivisibleBy year 400
+isLeapYear year = (year `isDivisibleBy` 4 && (not (year `isDivisibleBy` 100))) || year `isDivisibleBy` 400
