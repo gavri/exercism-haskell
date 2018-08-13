b73786e7e9a8175a496f0117275d5789add88625
Gavri Fernandez <gavri.fernandez@gmail.com>
2018-08-13 07:48:33 +0530

    Leap: Guard clauses

--- leap/src/LeapYear.hs
+++ leap/src/LeapYear.hs
@@ -3,4 +3,8 @@ module LeapYear (isLeapYear) where
 m `isDivisibleBy` n = mod m n == 0
 
 isLeapYear :: Integer -> Bool
-isLeapYear year = (year `isDivisibleBy` 4 && (not (year `isDivisibleBy` 100))) || year `isDivisibleBy` 400
+isLeapYear year
+        | year `isDivisibleBy` 400 = True
+        | year `isDivisibleBy` 100 = False
+        | year `isDivisibleBy` 4 = True
+        | otherwise = False
