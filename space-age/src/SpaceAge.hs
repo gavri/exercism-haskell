e57f012939b50a2ddb087c2d3b865602e36d1912
Gavri Fernandez <gavri.fernandez@gmail.com>
2018-08-14 07:47:02 +0530

    Space age: refactored

--- space-age/src/SpaceAge.hs
+++ space-age/src/SpaceAge.hs
@@ -1,5 +1,7 @@
 module SpaceAge (Planet(..), ageOn) where
 
+import Data.Maybe
+
 data Planet = Mercury
   | Venus
   | Earth
@@ -7,22 +9,23 @@ data Planet = Mercury
   | Jupiter
   | Saturn
   | Uranus
-  | Neptune
+  | Neptune deriving Eq
 
 orbitalPeriodInEarthYears :: Planet -> Float
-orbitalPeriodInEarthYears Mercury = 0.2408467
-orbitalPeriodInEarthYears Venus = 0.61519726
-orbitalPeriodInEarthYears Earth = 1.0
-orbitalPeriodInEarthYears Mars = 1.8808158
-orbitalPeriodInEarthYears Jupiter = 11.862615
-orbitalPeriodInEarthYears Saturn = 29.447498
-orbitalPeriodInEarthYears Uranus = 84.016846
-orbitalPeriodInEarthYears Neptune = 164.79132
+orbitalPeriodInEarthYears planet = fromMaybe 1.0 $ lookup planet orbitalPeriodInEarthYearsMap
+  where
+    orbitalPeriodInEarthYearsMap = [
+      (Mercury, 0.2408467),
+      (Venus, 0.61519726),
+      (Mars, 1.8808158),
+      (Jupiter, 11.862615),
+      (Saturn, 29.447498),
+      (Uranus, 84.016846),
+      (Neptune, 164.79132)]
 
 earthOrbitalPeriodInSeconds = 31557600
 
-orbitalPeriodInSeconds :: Planet -> Float
-orbitalPeriodInSeconds planet = (orbitalPeriodInEarthYears planet) * earthOrbitalPeriodInSeconds
-
 ageOn :: Planet -> Float -> Float
-ageOn planet seconds = seconds / (orbitalPeriodInSeconds planet)
+ageOn planet seconds = seconds / orbitalPeriodInSeconds
+  where
+    orbitalPeriodInSeconds = orbitalPeriodInEarthYears planet * earthOrbitalPeriodInSeconds
