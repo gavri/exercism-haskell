b5c6b8c786f9d901c2e82dc5113ee6f49997094a
Gavri Fernandez <gavri.fernandez@gmail.com>
2018-08-23 08:02:03 +0530

    Sum of multiples: more haskellish

--- sum-of-multiples/src/SumOfMultiples.hs
+++ sum-of-multiples/src/SumOfMultiples.hs
@@ -1,7 +1,4 @@
 module SumOfMultiples (sumOfMultiples) where
 
 sumOfMultiples :: [Integer] -> Integer -> Integer
-sumOfMultiples factors limit = sum $ multiplesToLimit factors limit
-  where multiplesToLimit factors limit = filter (isMultipleOfAnyOf factors) [1..limit - 1]
-        isMultipleOfAnyOf ns m = any (isMultipleOf m) ns
-        isMultipleOf m n = mod m n == 0
+sumOfMultiples factors limit = sum $ filter (\m -> any ((== 0) . (mod m)) factors) [1..limit - 1]
