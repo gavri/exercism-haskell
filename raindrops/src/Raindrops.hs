34b282aeb00d5ec5d53b89583d750bd46624b410
Gavri Fernandez <gavri.fernandez@gmail.com>
2018-09-04 08:15:05 +0530

    Raindrops: Move replaceWithRaindrop inside raindrops

--- raindrops/src/Raindrops.hs
+++ raindrops/src/Raindrops.hs
@@ -1,13 +1,11 @@
 module Raindrops (convert) where
 
-replaceWithRaindrop n m
-  | m == 3 && hasFactor = ("Pling" ++)
-  | m == 5 && hasFactor = ("Plang" ++)
-  | m == 7 && hasFactor = ("Plong" ++)
-  | otherwise  = id
-  where hasFactor = n `mod` m == 0
-
-raindrops n = (replaceWithRaindrop n 3 . replaceWithRaindrop n 5 . replaceWithRaindrop n 7) ""
+raindrops n = (replaceWithRaindrop 3 . replaceWithRaindrop 5 . replaceWithRaindrop 7) ""
+  where
+    replaceWithRaindrop 3 = if (hasFactor 3) then ("Pling" ++) else id
+    replaceWithRaindrop 5 = if (hasFactor 5) then ("Plang" ++) else id
+    replaceWithRaindrop 7 = if (hasFactor 7) then ("Plong" ++) else id
+    hasFactor m = n `mod` m == 0
 
 convert :: Int -> String
 convert n = if null result then show n else result
